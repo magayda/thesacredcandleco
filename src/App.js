0b610f028324e6ce7643147ad5b02bf4c02adcde
mg <magayda@email.uark.edu>
2020-10-08 23:06:37 -0500

    initialize

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="tsc-wrap">
+      <img src='badge-green-background@2x.png' className="tsc-logo" alt="logo" />
+      <div class="landing-message">
+        <h1> Website Coming Soon! </h1>
+        <h2> We're currently working to create an experience that allows our customers to find and order all of our luxury candles at the ease of your fingertips. We're excited for you to see all of our products in one place while still offering delivery and in-person pickup! </h2>
+        <h1> Get Social With Us! </h1>
+        <div class="social">
+          <a href="https://www.facebook.com/thesacredcandleco/"><img class="social-logo" src="fb-logo.png"/></a><h3>https://www.facebook.com/thesacredcandleco</h3>
+          <h3>https://www.instagram.com/thesacredcandleco</h3>
+        </div>
+        we appreciate // REVIEW:
+        use hashtags
+        * You can currently place orders through DM - Post with #ignitetheritual and #thesacredcandleco to enter to win a free candle!
+      </div>
+    </div>
+  );
+}
+
+export default App;
